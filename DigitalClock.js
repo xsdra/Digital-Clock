ba5b2ae29b17d9dac8e2a72d391f3aa063b2513a
xsdra <sandraxsdra@gmail.com>
2022-11-21 10:18:01 +0300

    digital clock

--- DigitalClock.js
+++ DigitalClock.js
@@ -13,6 +13,10 @@ function showTime(){
         h = h - 12;
         session = "PM";
     }
+    /**
+     *ternary operator (?)
+     
+     */
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
